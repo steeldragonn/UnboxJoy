9c5ac1d9b98d244436e2812cb1ddfec5a4fbff7d
Patricia Kroenert <p.kroenert98@gmail.com>
2023-12-11 13:45:19 +0100

    login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
-import "./App.css";
-import HomePage from "./pages/HomePage";
+import "./index.css";
+
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
