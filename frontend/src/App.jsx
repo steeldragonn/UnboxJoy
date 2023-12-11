97980d90ef0fb129231e9ed61b5bf8fd58b17eba
steeldragonn <sweetsourr.sos@gmail.com>
2023-12-11 15:19:09 -0800

    homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
