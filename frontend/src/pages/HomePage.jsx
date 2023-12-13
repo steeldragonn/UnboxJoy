d856ca613d3b91c68650175873f7df3516ba3c9a
steeldragonn <sweetsourr.sos@gmail.com>
2023-12-13 11:06:05 -0800

    done dont touch

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,52 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-// import Gift from "backend/models/Gift.model";
 import Navbar from "../components/Navbar";
 const API_URL = "http://localhost:5005";
 
 const HomePage = () => {
   const [gifts, setGifts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
-  // const [selectedNumberOfPeople, setSelectedNumberOfPeople] = useState("");
-
   const [query, setQuery] = useState("");
   const [filteredArray, setFilteredArray] = useState([]);
-  const [filteredGifts, setFilteredGifts] = useState([]);
 
   useEffect(() => {
     // create a api call to the backend which is receiving all the gifts.json file from backend
     const params = {};
 
-    // if (selectedCategory) {
-    //   params.category = selectedCategory;
-    // }
-    // if (selectedNumberOfPeople) {
-    //   params.numberOfPeople = selectedNumberOfPeople;
-    // }
-    // console.log(params);
-
     axios
-      .get(`${API_URL}/gifts`)
+      .get(`${API_URL}/gifts`, { params })
       .then((response) => {
         setGifts(response.data);
         setFilteredArray(response.data);
-        console.log(response.data);
-        const allGifts = response.data;
-        setGifts(allGifts);
-        setFilteredGifts(allGifts);
+        console.log(response);
       })
       .catch((error) => console.error("Error fetching gifts", error));
   }, []);
 
-  const handleFilterChange = (filters) => {
-    let filteredResults = gifts;
-    if (filters.category) {
-      filteredResults = filteredResults.filter((gift) =>
-        gift.category.includes(filters.category)
-      );
+  useEffect(() => {
+    if (selectedCategory && query) {
+      setFilteredArray(handleFiltering());
+    } else {
+      setFilteredArray(gifts);
     }
-    setFilteredGifts(filteredResults);
+  }, [query, selectedCategory, gifts]);
+  console.log(
+    "THIS IS WHAT I AM SEARCHING FOR WITH CATEGORY  =>",
+    selectedCategory
+  );
+  console.log(
+    "THIS IS WHAT I AM SEARCHING FOR WITH NUMBER OF PEOPLE =>",
+    query
+  );
+
+  const handleFiltering = () => {
+    return gifts.filter((eachGift) => {
+      return (
+        (selectedCategory === "" ||
+          eachGift.category.includes(selectedCategory)) &&
+        eachGift.numberOfPeople === query
+      );
+    });
   };
 
   return (
@@ -55,9 +56,37 @@ const HomePage = () => {
       <h1>UnboxJoy</h1>
       <div>
         <h2>Choose joy for loved ones</h2>
-        <ul>
-          {gifts.map((gift) => (
-            <li key={gift._id}>
+        <label>
+          Category:
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="art">art</option>
+            <option value="wellness">wellness </option>
+            <option value="adrenaline">adrenaline </option>
+            <option value="indoor">indoor</option>
+            <option value="outdoor">outdoor</option>
+            <option value="food">food</option>
+            <option value="trip">trip</option>
+            <option value="sport">sport</option>
+            <option value="music">music</option>
+          </select>
+        </label>
+
+        <label>
+          Number of People:
+          <input
+            type="number"
+            value={query}
+            onChange={(e) => setQuery(Number(e.target.value))}
+          />
+        </label>
+
+        <ul className="giftList">
+          {filteredArray.map((gift) => (
+            <li key={gift._id} className="giftItem">
               <div>
                 <img
                   src={gift.imageURL}
